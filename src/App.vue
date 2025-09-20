d8cce73c9742f3e07013f3ccd2b97623d503b3c2
Заславский Ярослав <blackfyz@yandex.ru>
2025-09-20 11:49:41 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import ToDoItem from './components/ToDoItem.vue'
+const tasks = ref<string[]>([
+  'task1',
+  'task2',
+  'task3',
+])
+
+const inputValue = ref<string>('')
+
+const onCLick = () => {
+  if (inputValue.value === ''){
+    return
+  }
+  tasks.value.push(inputValue.value)
+  inputValue.value = ''
+  }
+</script>
+
+<template>
+  <input type = "text" v-model = "inputValue">
+  <button @click = "onCLick">Добавить задачу</button>
+
+  <hr>
+  <ul>
+    <ToDoItem
+      v-for = "task in tasks"
+      :key = "task"
+      :task= "task"
+      ></ToDoItem>
+  </ul>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,13 @@
+<script setup lang ="ts">
+const props = defineProps<{
+  task: string
+}>()
+</script>
+
+<template>
+  <li>
+    {{props.task}}
+  </li>
+</template>
+
+<style scoped></style>
